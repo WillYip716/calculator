3b04cfdb4fcfca88e346174bd3ad349f1327fa73
WillYip716 <william.yip.716@gmail.com>
2020-04-23 17:43:14 -0700

    repeated calc and remove last function

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ let lastNum, lastOp, lastButton;
 let num = function(val){
     let holder = document.getElementById("viewPort");
 
-    if(lastButton=="operator"){
+    if(lastButton=="calc"){
+        reset(val);
+    }
+    else if(lastButton=="operator"){
         lastNum = parseInt(holder.innerHTML);
         holder.innerHTML = val;
     }
@@ -26,19 +29,15 @@ let op = function(val){
     switch (val) {
         case "+":
             document.getElementById("plus").classList.add("selected");
-            lastOp = "plus";
             break;
         case "-":
             document.getElementById("minus").classList.add("selected");
-            lastOp = "minus";
             break;
         case "/":
             document.getElementById("divide").classList.add("selected");
-            lastOp = "divide";
             break;
         case "*":
             document.getElementById("multiply").classList.add("selected");
-            lastOp = "multiply";
             break;
         default:
             break;
@@ -47,11 +46,11 @@ let op = function(val){
     lastButton = "operator";
 }
 
-let reset = function(){
+let reset = function(val){
     lastButton = null;
     lastNum = null;
     lastOp = null;
-    document.getElementById("viewPort").innerHTML = "0";
+    document.getElementById("viewPort").innerHTML = "" + val;
     let opList = document.querySelector(".selected");
     if(opList){
         opList.classList.remove("selected");
@@ -59,37 +58,75 @@ let reset = function(){
 }
 
 let calc = function(){
-    let answer;
+
+    let temp;
     let view = document.getElementById("viewPort");
     let operator = document.querySelector(".selected");
-    if(operator&&operator.id=="divide"&&view.innerHTML == "0"){}
-    else if(operator){
-        switch (operator.id) {
+    if(lastButton == "calc"){
+        console.log("lastNum is " +lastNum)
+        switch (lastOp) {
             case "plus":
-                view.innerHTML = lastNum + parseInt(view.innerHTML);
+                view.innerHTML = parseInt(view.innerHTML) + lastNum ;
                 lastOp = "plus";
                 break;
             case "minus":
-                view.innerHTML = lastNum - parseInt(view.innerHTML);
+                view.innerHTML = parseInt(view.innerHTML) - lastNum ;
                 lastOp = "minus";
                 break;
             case "divide":
-                if(view.innerHTML == "0"){
-                    break;
-                }
-                view.innerHTML = lastNum / parseInt(view.innerHTML);
+                view.innerHTML = parseInt(view.innerHTML) / lastNum ;
                 lastOp = "divide";
                 break;
             case "multiply":
-                view.innerHTML = lastNum * parseInt(view.innerHTML);
+                view.innerHTML = parseInt(view.innerHTML) * lastNum ;
                 lastOp = "multiply";
                 break;
             default:
                 break;
         }
-        lastButton = "calc";
-        operator.classList.remove("selected");
-
     }
+    else if(operator&&operator.id=="divide"&&view.innerHTML == "0"){}
+    else if(operator){
+        temp = parseInt(view.innerHTML);
+        switch (operator.id) {
+            case "plus":
+                view.innerHTML = lastNum + parseInt(view.innerHTML);
+                lastOp = "plus";
+                lastNum = temp;
+                break;
+            case "minus":
+                view.innerHTML = lastNum - parseInt(view.innerHTML);
+                lastOp = "minus";
+                lastNum = temp;
+                break;
+            case "divide":
+                view.innerHTML = lastNum / parseInt(view.innerHTML);
+                lastOp = "divide";
+                lastNum = temp;
+                break;
+            case "multiply":
+                view.innerHTML = lastNum * parseInt(view.innerHTML);
+                lastOp = "multiply";
+                lastNum = temp;
+                break;
+            default:
+                break;
+        }
+        if(!(lastButton=="calc")){
+            lastButton = "calc";
+            operator.classList.remove("selected");
+        }
+    }
+}
 
+let removeLast = function(){
+    let holder = document.getElementById("viewPort");
+    if(!(holder.innerHTML == "0")){
+        if(holder.innerHTML.length==1){
+            holder.innerHTML = 0;
+        }
+        else{
+            holder.innerHTML = holder.innerHTML.substring(0,holder.innerHTML.length-1);
+        }
+    }
 }
